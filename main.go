ab0bf9b9b3acc19971928e6fb511c7b15e2d1177
BoraCakmaz <34280778+boracakmazz@users.noreply.github.com>
2021-12-21 16:34:08 +0300

    handlers v1.0

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Microservices with Go")
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(rw, "Ooops somewthing went wrong", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw, "Helloooo %s", d)
+	})
+
+	http.HandleFunc("/exit", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Exit endpoint")
+		fmt.Fprintf(rw, "Exit endpoint reached")
 	})
 
 	http.ListenAndServe(":8080", nil)
